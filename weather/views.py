ce925100fb71670e16a3ae5ec1981aff83acac43
khushi <khushi@gmail.com>
2024-02-11 18:02:34 +0530

    authentication

--- weather/views.py
+++ weather/views.py
@@ -1,8 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout 
+from django.contrib import messages
 import json
 import urllib.request
 
-# Create your views here.
+def register(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        email=request.POST['email']
+        password=request.POST['password']
+        password2=request.POST['password2']
+        if password==password2:
+            if User.objects.filter(email=email).exists():
+                messages.success(request,'Email Already Exist')
+                return redirect('register')
+            elif User.objects.filter(username=username).exists():
+                messages.success(request,'Username already exist')
+                return redirect('register')
+            else:
+                user=User.objects.create_user(username=username,email=email,password=password)
+                user.save()
+                return redirect('login')
+        else:
+            messages.success(request,"Password not the same")
+            return redirect('register')    
+    else:
+        return render(request,'register.html')
+    
+def login_user(request):
+    if request.method == 'POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        user=authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('index')
+        else:
+            messages.success(request,'Invalid Credentials')
+            return redirect('login')
+    else:    
+        return render(request,"login.html")    
+            
 def index(request):
     if request.method == 'POST':
         city = request.POST['city']
@@ -16,9 +55,11 @@ def index(request):
             "pressure": str(json_data['main']['pressure']),
             "humidity": str(json_data['main']['humidity']),
         }
-
     else:
         city = ''
         data = {}
     return render(request, 'index.html', {'city': city, 'data': data})
 
+def logout_user(request):
+    logout(request)
+    return redirect('login')
